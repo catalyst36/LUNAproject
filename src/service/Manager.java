ba1b68ea152e4df1a2dcb758b09befb879218a63
HOGYUMKIM <hogyum56@gmail.com>
2022-06-16 17:28:30 +0900

    sd

--- src/service/Manager.java
+++ src/service/Manager.java
@@ -1,25 +1,24 @@
 package service;
 
 import java.io.IOException;
-import java.util.Scanner;
-
 import dao.book.BookStockDAO;
 import dao.login.ManagerLoginDAO;
 import dao.manager.ManagerListDAO1;
 import dao.manager.ManagerListDAO2;
 import dao.manager.ManagerListDAO3;
+import vo.EmployeeVO;
+import util.BufferUtil;
 
 public class Manager {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Manager manager = new Manager();
 
 		manager.login();  // 관리자 페이지 접속하자마자 로그인 메소드 호출
 		
 	}
 	
-	public void login(){ //  로그인 메소드
-		Scanner sc = new Scanner(System.in);
+	public void login() throws Exception{ //  로그인 메소드
 		Manager manager = new Manager();
 		ManagerLoginDAO managerLogin = new ManagerLoginDAO();
 		boolean run = false;
@@ -28,12 +27,12 @@ public class Manager {
 		while(true){
 			System.out.println("|--------------관리자 로그인---------------|");
 			System.out.print("ID : ");
-			String id = sc.next();
+			String id = BufferUtil.readLine();
 			System.out.print("PW : ");
-			String pw = sc.next();
+			String pw = BufferUtil.readLine();
 			
-			managerLogin.ManagerLogin();
-			if (id.equals(managerLogin.getEmpID()) && pw.equals(managerLogin.getEmpPW())){
+			EmployeeVO vo = managerLogin.empLogin(id, pw);
+			if (vo != null){
 					System.out.println("로그인에 성공하였습니다.");
 					System.out.println("|----------------------------------------------|");
 					System.out.println();
@@ -61,16 +60,15 @@ public class Manager {
 		}
 	}
 	
-	public void exe(){ // 실행 메소드
-		Scanner sc = new Scanner(System.in);
+	public void exe() throws Exception{ // 실행 메소드
 		Manager manager = new Manager();
 		boolean run = false; // while문 안쓸거면 지우기
 		
-		System.out.println("================================================");
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 		System.out.println("| 1. 책 입고  |  2. 회원 목록 조회 |  3. 도서 목록 조회  |  4. 로그아웃  |");
-		System.out.println("================================================");
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 		System.out.print("사용할 기능 입력 : ");
-		int num = sc.nextInt();
+		int num = BufferUtil.nextInt();
 		
 		switch(num){
 		case 1:
@@ -84,26 +82,25 @@ public class Manager {
 		}
 	}
 	
-	public void bookStock(){  // 책 입고 메소드
+	public void bookStock() throws Exception{  // 책 입고 메소드
 		BookStockDAO stock = new BookStockDAO();
 		stock.insertBook();
 	}
 	
-	public void memberList(){ // 회원 목록 조회 메소드
+	public void memberList() throws Exception{ // 회원 목록 조회 메소드
 		ManagerListDAO1 memList = new ManagerListDAO1();
 		memList.selectMember();
 	}
 	
-	public void bookList(){ // 도서 목록 조회 메소드
+	public void bookList() throws Exception{ // 도서 목록 조회 메소드
 		Manager manager = new Manager();
-		Scanner sc = new Scanner(System.in);
 		ManagerListDAO2 bookList = new ManagerListDAO2();
 		
 		bookList.bookSelect();
-		System.out.println("================d======================");
-		System.out.println("| 1. 품절 품목 추가 입고    |   2. 뒤로 가기                |");
-		System.out.println("======================================");
-		int num = sc.nextInt();
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		System.out.println("| 1. 품절 품목 추가 입고   | 2. 뒤로 가기      |");
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		int num = BufferUtil.nextInt();
 		
 		if (num == 1){
 			ManagerListDAO3 bookList2 = new ManagerListDAO3();
@@ -114,7 +111,7 @@ public class Manager {
 		
 	}
 	
-	public void logout(){
+	public void logout() throws Exception{
 		System.out.println("로그아웃 되었습니다");
 		try {
 			Main.main(null); // 로그아웃 시 메인 화면으로 이동
