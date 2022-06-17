67db5cbe23c7a44dbcc9550b7c4fff5f0ad60932
YUHWALEE <cisa7043@naver.com>
2022-06-17 20:22:08 +0900

    ManagerListDAO3 UI 수정 완료

--- src/dao/manager/ManagerListDAO3.java
+++ src/dao/manager/ManagerListDAO3.java
@@ -34,12 +34,16 @@ public class ManagerListDAO3 {
 			rs = stmt.executeQuery(sql);
 			
 			if (rs.isBeforeFirst() == false ){
-				System.out.println("품절된 도서가 없습니다.");
+		           System.out.printf("┌%97s┐\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+		           System.out.printf("%52s\n", "품절된 도서가 없습니다.");
+		           System.out.printf("└%97s┘\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+
 				manager.exe();
 			}else{
 			
-				System.out.println("책코드\t\t책 이름\t\t판매가\t\t책 저자\t\t출판사\t\t책 수량\t\t장르\t\t판매수량");
-				System.out.println("====================================================================");
+				System.out.printf("\n %-5s %-50s %-5s %-15s %-15s %-3s %-9s %-6s\n"
+						, "코드", "제목", "가격", "저자", "출판사", "수량", "장르", "누적판매수");
+		           System.out.printf("%120s\n","─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
 				while(rs.next()){
 					String book_id = SEQ.createSequenceKey(rs.getString("book_id")); // DB의 책코드를 문자열로 변환 (65000 -> A000)
 					String book_name = rs.getString("book_name");
@@ -50,25 +54,28 @@ public class ManagerListDAO3 {
 					String book_genre = rs.getString("book_genre");
 					String book_qtysale = rs.getString("book_qtysale");
 
-					System.out.println(book_id + "\t\t" + book_name + "\t\t" + book_sale + "\t\t"
-					+ book_author + "\t\t" + book_pub + "\t\t" + book_qty + "\t\t" + book_genre  + "\t\t" +	book_qtysale);
+					System.out.printf("\n %-5s %-50s %-5s %-15s %-15s %-3s %-9s %-6s\n",
+							book_id, book_name, book_sale, book_author, book_pub, book_qty
+								, book_genre, book_qtysale);
 					
 
 			} 
-				System.out.println("====================================================================");
-
+		           System.out.printf("%120s\n","─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
+		           System.out.println();
 				StringBuilder sql1 = new StringBuilder();
 			
 				sql1.append("update book set  book_qty = ?  where book_id = ?");
 
-				System.out.print("책 코드 입력 [대소문자 구분] : ");
+		        System.out.printf("┌%97s┐\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+				System.out.printf("%25s","책 코드 입력 [대소문자 구분] : ");
 				String bid = BufferUtil.readLine();
-				System.out.print("입고할 수량 입력 : ");
-				int  bqty = BufferUtil.nextInt();
+				System.out.printf("%25s","입고할 수량 입력 : ");
+				int  bqty = BufferUtil.nextInt();	
+				System.out.printf("└%97s┘\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+
 			
 				int bid_final = SEQ.returnSequenceKey(bid); // 입력받은 문자열을 숫자로 변환하여 bid_final에 저장
 				String bid_fin = Integer.toString(bid_final); // BOOK_ID 컬럼이 VARCHAR2이므로 bid_final을 문자열로 변환 
-				System.out.println(bid_final); // 완료되면 지우기
 			
 				pstmt = conn.prepareStatement(sql1.toString());
 				pstmt.setInt(1, bqty);
@@ -77,13 +84,15 @@ public class ManagerListDAO3 {
 				int res =pstmt.executeUpdate();
 				
 				conn.commit();
-				
-				System.out.println(res);
 			
 				if (res ==0){
-					System.out.println("자료 입력에 실패하였습니다.");
+			        System.out.printf("┌%97s┐\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+					System.out.printf("%52\n","자료 입력에 실패하였습니다.");
+					System.out.printf("└%97s┘\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
 				} else{
-					System.out.println("책코드가 [" + bid + "] 인 도서가 " + bqty + "권 추가 입고 되었습니다.");
+			        System.out.printf("┌%97s┐\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
+					System.out.println("      책코드가 [" + bid + "] 인 도서가 " + bqty + "권 추가 입고 되었습니다.");
+					System.out.printf("└%97s┘\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
 				}
 			
 			}
