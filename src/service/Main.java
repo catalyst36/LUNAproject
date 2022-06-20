aec73354a85ee027eeaf85c4189c37a463429df3
silberRain <kbee_5@naver.com>
2022-06-20 09:31:42 +0900

    마이페이지에서 뒤로가기 추가

--- src/service/Main.java
+++ src/service/Main.java
@@ -11,7 +11,8 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		while (true) {
-			
+			System.out.printf("\n\n\n\n\n\n");
+
            System.out.printf("┌%97s┐\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
            System.out.printf("%20s %18s %18s %18s\n","1.회원가입","2.로그인","3.관리자","4.종료");
            System.out.printf("└%97s┘\n","─────────────────────────────────────────────────────────────────────────────────────────────────");
